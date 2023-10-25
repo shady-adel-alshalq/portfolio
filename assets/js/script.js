261f95e77f2853419f246c58a72071b99fe717ef
shady-adel-alshalq <134493220+shady-adel-alshalq@users.noreply.github.com>
2023-10-25 13:31:55 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Instructor information technology", "Training program coordinator",  "", ""],
+    strings: ["Instructor information technology", "Coordinator for training programs",  "", ""],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
